b630791e68dd5cd209d04985547cf9a78971cff8
MRUDULA N NAIK <MRUDULA N NAIK@LAPTOP-02J0H6PP>
2021-04-07 10:46:47 +0530

    transient demo

--- collections/src/serialisation/Dog.java
+++ collections/src/serialisation/Dog.java
@@ -1,19 +1,12 @@
 package serialisation;
 
+
 import java.io.Serializable;
 
 public class Dog implements Serializable {
-	public static void main(String[] args) {
+	 
+		transient  int i = 10;
+		int j = 50;
 		
 	
-	int i = 10;
-	int j = 20;
-	
-	}
-
-	public int j = 10  ;
-	public int i = 20;
-	
-	
-
 }
