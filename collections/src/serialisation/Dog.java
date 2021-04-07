e18038d14797f013f46bdb1fbebdb5131be1163b
MRUDULA N NAIK <MRUDULA N NAIK@LAPTOP-02J0H6PP>
2021-04-07 10:40:39 +0530

    Serialisation

---
+++ collections/src/serialisation/Dog.java
@@ -0,0 +1,19 @@
+package serialisation;
+
+import java.io.Serializable;
+
+public class Dog implements Serializable {
+	public static void main(String[] args) {
+		
+	
+	int i = 10;
+	int j = 20;
+	
+	}
+
+	public int j = 10  ;
+	public int i = 20;
+	
+	
+
+}
