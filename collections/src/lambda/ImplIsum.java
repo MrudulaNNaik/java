982a4f82d313c556420d5bb5a2dc9757176d2a70
MRUDULA N NAIK <MRUDULA N NAIK@LAPTOP-02J0H6PP>
2021-04-08 11:34:28 +0530

    lambda

---
+++ collections/src/lambda/ImplIsum.java
@@ -0,0 +1,11 @@
+package lambda;
+
+public class ImplIsum implements ISum {
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+}
---
+++ collections/src/lambda/LifeB4FuncInterface.java
@@ -0,0 +1,9 @@
+package lambda;
+
+public class LifeB4FuncInterface {
+	public static void main(String[] args) {
+		ImplIsum isum = new ImplIsum();
+		System.out.println(isum.add(10,20));
+	}
+
+}
