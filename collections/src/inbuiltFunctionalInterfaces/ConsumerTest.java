2012b0beb619093fb4004aec20f3960819331a56
MRUDULA N NAIK <MRUDULA N NAIK@LAPTOP-02J0H6PP>
2021-04-09 12:57:37 +0530

    Supplier

--- collections/src/inbuiltFunctionalInterfaces/ConsumerTest.java
+++ collections/src/inbuiltFunctionalInterfaces/ConsumerTest.java
@@ -2,13 +2,16 @@ package inbuiltFunctionalInterfaces;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class ConsumerTest {
 	static ArrayList<JavaStudent> javaStudents = StudentTest.populateStudents();
 	
 	public static void main(String[] args) {
 		Consumer<JavaStudent> c= s -> System.out.println(s.name);
+		Predicate<JavaStudent> p = s -> s.marks >= 70;
 		
+		System.out.println();
 		for(JavaStudent student: javaStudents){
 			c.accept(student);
 			
