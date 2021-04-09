74fe9822879f33e5b44643774b1be184a6831108
MRUDULA N NAIK <MRUDULA N NAIK@LAPTOP-02J0H6PP>
2021-04-09 10:36:38 +0530

    static and predicate

---
+++ collections/src/inbuiltFunctionalInterfaces/TestPredicate.java
@@ -0,0 +1,13 @@
+package inbuiltFunctionalInterfaces;
+
+import java.util.function.Predicate;
+
+public class TestPredicate {
+	public static void main(String[] args) {
+		Predicate<Integer> p = i -> i % 2 == 0;
+		System.out.println(p.test(10));
+	
+		
+	}
+
+}
---
+++ collections/src/lambda/defaultstatic/Student.java
@@ -0,0 +1,9 @@
+package lambda.defaultstatic;
+
+public class Student {
+	public static void main(String[] args) {
+		System.out.println(IUtility.isEligible(16));
+
+	}
+
+}
