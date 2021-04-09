a810466c8154bc67c1ebce414c48cb5e22cfa934
MRUDULA N NAIK <MRUDULA N NAIK@LAPTOP-02J0H6PP>
2021-04-09 16:38:32 +0530

    Eligibility

---
+++ collections/src/inbuiltFunctionalInterfacesBiparams/CheckEligibility.java
@@ -0,0 +1,22 @@
+package inbuiltFunctionalInterfacesBiparams;
+
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+
+public class CheckEligibility {
+	public static void main(String[] args) {
+		BiFunction<Integer, Integer, Pupil> bif = (a,m) -> new Pupil("Naik", a,m);
+		
+		Pupil pupil = bif.apply(19,82);
+		BiPredicate<Integer,Integer> bip1 = (age,marks) -> (age >= 18);
+		BiPredicate<Integer,Integer> bip2 = (age,marks) -> (marks > 60);
+		if (bip1. and (bip2).test(pupil.age, pupil.marks)) {
+			System.out.println(pupil.name+" elgible");
+		}
+		else {
+			System.out.println(pupil.name+" not elgible");
+		}
+			
+	}
+
+}
---
+++ collections/src/inbuiltFunctionalInterfacesBiparams/Pupil.java
@@ -0,0 +1,16 @@
+package inbuiltFunctionalInterfacesBiparams;
+
+public class Pupil {
+	
+	String name;
+	int age ;
+	int marks;
+	
+	public Pupil(String name, int age, int marks) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.marks = marks;
+	}
+
+}
