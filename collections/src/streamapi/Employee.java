3f939087af28b481c55c154009e92899f682a3aa
MRUDULA N NAIK <MRUDULA N NAIK@LAPTOP-02J0H6PP>
2021-04-12 15:30:56 +0530

    min and max

--- collections/src/streamapi/Employee.java
+++ collections/src/streamapi/Employee.java
@@ -1,25 +1,27 @@
 package streamapi;
 
-public class Employee{ 
-// implements Comparable<Employee> {
+public class Employee implements Comparable<Employee> {
 	String name;
 	int id;
-	public Employee(String name, int id) {
+	int salary;
+	
+	public Employee(String name, int id, int salary) {
 		super();
 		this.name = name;
 		this.id = id;
+		this.salary = salary;
 	}
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return this.name + " " + this.id;
+		return this.name + " " + this.id + " "+ this.salary;
 	} 
 	
-	//@Override
-	//public int compareTo(Employee o) {
-		//return (this.id  > o.id)? 1
-			//	: (this.id < o.id)?-1 : 0;
-	//}
+	@Override
+	public int compareTo(Employee o) {
+		return (this.salary  > o.salary)? -1
+				: (this.salary < o.salary)? 1 : 0;
+	}
 	
 	
 
--- collections/src/streamapi/EmployeeSort.java
+++ collections/src/streamapi/EmployeeSort.java
@@ -8,11 +8,11 @@ import java.util.stream.Collectors;
 public class EmployeeSort {
 	public static void main(String[] args) {
 		ArrayList<Employee> emplList = new ArrayList<Employee>();
-		emplList.add(new Employee("naik", 43) );
-		emplList.add(new Employee("poorna", 59) );
-		emplList.add(new Employee("madhu",39) );
-		emplList.add(new Employee("yashu", 51) );
-		emplList.add(new Employee("anu", 8) );
+		emplList.add(new Employee("naik", 43, 4587) );
+		emplList.add(new Employee("poorna", 59, 5022) );
+		emplList.add(new Employee("madhu",39, 1255) );
+		emplList.add(new Employee("yashu", 51, 4562) );
+		emplList.add(new Employee("anu", 8, 2548) );
 		
 		System.out.println(emplList);
 		//List<Employee> sortedList = emplList.stream().sorted().collect(Collectors.toList())	;	
@@ -22,10 +22,13 @@ public class EmployeeSort {
 				0;
 		};
 		List<Employee> sortedList = emplList.stream().sorted(c).collect(Collectors.toList())	;	
-		
-		
-		
 		System.out.println(sortedList);
+		
+		Employee idmax = emplList.stream().max((e1,e2) -> e2.compareTo(e1)).get();
+		System.out.println("Employee with max salary: " +idmax.toString());
+		
+		Employee idmin = emplList.stream().min((e1,e2) -> e2.compareTo(e1)).get();
+		System.out.println("Employee with min salary: " + idmin.toString());
 		
 	}
 
